d7492fb43b75f38f54aed225cdef983e55c94bc2
Carl Stanley <49922081+Carl-Stanley@users.noreply.github.com>
2019-09-13 22:56:46 -0700

    commit

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -6,8 +6,7 @@ const Header = () => {
     return (
 
          <header className="App-header">
-        <img className="wc" src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/2019_FIFA_Women%27s_World_Cup.svg/1280px-2019_FIFA_Women%27s_World_Cup.svg.png' alt='#'></img><h3>Women's World Cup</h3>
-        <img className="wc" src='https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/2019_FIFA_Women%27s_World_Cup.svg/1280px-2019_FIFA_Women%27s_World_Cup.svg.png' alt='#'></img>
+        <img className="wc" src='.fifa.jpg' alt='#'></img>
          </header>
 
     )
